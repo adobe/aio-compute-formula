89adddbc7fd21718a2ea7743c1925edc21eca778
kelkingt <kelkingt@adobe.com>
2023-09-14 11:27:13 -0700

    render-manifest fix

--- scripts/renderManifest.js
+++ scripts/renderManifest.js
@@ -1,12 +1,13 @@
 const {renderManifest} = require("./manifest.js")
 
-const yargs = require('yargs/yargs')(process.argv.slice(2)).argv
+// const yargs = require('yargs/yargs')(process.argv.slice(2)).argv
 
 function main(path, apiKey, logLevel) {
-    console.log(yargs)
-    console.log(`render manifest params: path-${path}, key-${apiKey}, log-${logLevel}`)
+    // console.log(yargs)
+    // console.log(`render manifest params: path-${path}, key-${apiKey}, log-${logLevel}`)
     renderManifest(path, apiKey, logLevel)
 }
 
 // main(yargs.path, yargs.apiKey, yargs.logLevel)
-main(process.env.npm_config_path, process.env.npm_config_apiKey, process.env.npm_config_logLevel)
+// main(process.env.npm_config_path, process.env.npm_config_apiKey, process.env.npm_config_logLevel)
+main(process.argv[2], process.argv[3], process.argv[4])
